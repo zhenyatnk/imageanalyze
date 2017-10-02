d86e0ab2abdc20578d5f8684af68b6b1723caeba
zhenaytnk <tinkalyk@mail.ru>
2017-10-02 20:47:09 +0300

    - Добавлены фабричные методы для создания из HEX - Добавлены методы получение hex

--- core/include/imageanalyzer/core/TColor.hpp
+++ core/include/imageanalyzer/core/TColor.hpp
@@ -3,17 +3,21 @@
 namespace imageanalyzer {
 namespace core {
 
+#define TO_BYTE(value, number) (value >> number*8) & 0xff
+
 struct TColor
 {
+    using Byte = unsigned char;
+
     TColor()
         :m_Alpha(255), m_Red(0), m_Green(0), m_Blue(0)
     {}
 
-    TColor(unsigned char aRed, unsigned char aGreen, unsigned char aBlue)
+    TColor(Byte aRed, Byte aGreen, Byte aBlue)
         :m_Alpha(255), m_Red(aRed), m_Green(aGreen), m_Blue(aBlue)
     {}
 
-    TColor(unsigned char aAlpha, unsigned char aRed, unsigned char aGreen, unsigned char aBlue)
+    TColor(Byte aAlpha, Byte aRed, Byte aGreen, Byte aBlue)
         :m_Alpha(aAlpha), m_Red(aRed), m_Green(aGreen), m_Blue(aBlue)
     {}
 
@@ -25,10 +29,38 @@ struct TColor
                m_Blue  == aRht.m_Blue;
     }
 
-    unsigned char m_Alpha;
-    unsigned char m_Red;
-    unsigned char m_Green;
-    unsigned char m_Blue;
+    unsigned long To_RGB()
+    {
+        return ((m_Red & 0xff) << 2*8) + ((m_Green & 0xff) << 1*8) + (m_Blue & 0xff);
+    }
+
+    unsigned long To_RGBA()
+    {
+        return ((m_Red & 0xff) << 3*8) + ((m_Green & 0xff) << 2*8) + ((m_Blue & 0xff)<< 1*8) + (m_Alpha & 0xff);
+    }
+
+    unsigned long To_ARGB()
+    {
+        return ((m_Alpha & 0xff) << 3*8) + ((m_Red & 0xff) << 2*8) + ((m_Green & 0xff) << 1*8) + (m_Blue & 0xff);
+    }
+    //------------------------------------------------------------
+    static TColor FromRGBA(unsigned long aValue)
+    {
+        return TColor(TO_BYTE(aValue, 0), TO_BYTE(aValue, 3), TO_BYTE(aValue, 2), TO_BYTE(aValue, 1));
+    }
+    static TColor FromARGB(unsigned long aValue)
+    {
+        return TColor(TO_BYTE(aValue, 3), TO_BYTE(aValue, 2), TO_BYTE(aValue, 1), TO_BYTE(aValue, 0));
+    }
+    static TColor FromRGB(unsigned long aValue)
+    {
+        return TColor(TO_BYTE(aValue, 2), TO_BYTE(aValue, 1), TO_BYTE(aValue, 0));
+    }
+
+    Byte m_Alpha;
+    Byte m_Red;
+    Byte m_Green;
+    Byte m_Blue;
 };
 
 }
--- core/test/TColor_test.cpp
+++ core/test/TColor_test.cpp
@@ -56,3 +56,33 @@ TEST_F(TColor_test, operator_notequal_4)
 {
     ASSERT_EQ(false, TColor(0, 1, 2, 3) == TColor(255, 1, 2, 3));
 }
+
+TEST_F(TColor_test, To_RGB)
+{
+    ASSERT_EQ(0xFF7F50, TColor(255, 127, 80).To_RGB());
+}
+
+TEST_F(TColor_test, To_RGBA)
+{
+    ASSERT_EQ(0xFF7F5080, TColor(128, 255, 127, 80).To_RGBA());
+}
+
+TEST_F(TColor_test, To_ARGB)
+{
+    ASSERT_EQ(0x80FF7F50, TColor(128, 255, 127, 80).To_ARGB());
+}
+
+TEST_F(TColor_test, From_RGB)
+{
+    ASSERT_EQ(TColor(255, 127, 80), TColor::FromRGB(0xFF7F50));
+}
+
+TEST_F(TColor_test, From_RGBA)
+{
+    ASSERT_EQ(TColor(128, 255, 127, 80), TColor::FromRGBA(0xFF7F5080));
+}
+
+TEST_F(TColor_test, From_ARGB)
+{
+    ASSERT_EQ(TColor(128, 255, 127, 80), TColor::FromARGB(0x80FF7F50));
+}
